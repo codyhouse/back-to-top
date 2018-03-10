2aa23dcbcb02036ff4909a9660b78d04b169296e
claudia-romano <claudia.romano06@gmail.com>
2018-03-10 14:38:37 +0000

    Remove bourbon dependency
    - Remove bourbon dependency
    - css/js improvements

--- js/main.js
+++ js/main.js
@@ -1,28 +1,75 @@
-jQuery(document).ready(function($){
-	// browser window scroll (in pixels) after which the "back to top" link is shown
-	var offset = 300,
+(function(){
+	var backTop = document.getElementsByClassName('js-cd-top')[0],
+		// browser window scroll (in pixels) after which the "back to top" link is shown
+		offset = 300,
 		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
-		offset_opacity = 1200,
-		//duration of the top scrolling animation (in ms)
-		scroll_top_duration = 700,
-		//grab the "back to top" link
-		$back_to_top = $('.cd-top');
+		offsetOpacity = 1200,
+		scrollDuration = 700
+		scrolling = false;
+	if( backTop ) {
+		//update back to top visibility on scrolling
+		window.addEventListener("scroll", function(event) {
+			if( !scrolling ) {
+				scrolling = true;
+				(!window.requestAnimationFrame) ? setTimeout(checkBackToTop, 250) : window.requestAnimationFrame(checkBackToTop);
+			}
+		});
+		//smooth scroll to top
+		backTop.addEventListener('click', function(event) {
+			event.preventDefault();
+			(!window.requestAnimationFrame) ? window.scrollTo(0, 0) : scrollTop(scrollDuration);
+		});
+	}
 
-	//hide or show the "back to top" link
-	$(window).scroll(function(){
-		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
-		if( $(this).scrollTop() > offset_opacity ) { 
-			$back_to_top.addClass('cd-fade-out');
-		}
-	});
+	function checkBackToTop() {
+		var windowTop = window.scrollY || document.documentElement.scrollTop;
+		( windowTop > offset ) ? addClass(backTop, 'cd-top--show') : removeClass(backTop, 'cd-top--show', 'cd-top--fade-out');
+		( windowTop > offsetOpacity ) && addClass(backTop, 'cd-top--fade-out');
+		scrolling = false;
+	}
+	
+	function scrollTop(duration) {
+	    var start = window.scrollY || document.documentElement.scrollTop,
+	        currentTime = null;
+	        
+	    var animateScroll = function(timestamp){
+	    	if (!currentTime) currentTime = timestamp;        
+	        var progress = timestamp - currentTime;
+	        var val = Math.max(Math.easeInOutQuad(progress, start, -start, duration), 0);
+	        window.scrollTo(0, val);
+	        if(progress < duration) {
+	            window.requestAnimationFrame(animateScroll);
+	        }
+	    };
 
-	//smooth scroll to top
-	$back_to_top.on('click', function(event){
-		event.preventDefault();
-		$('body,html').animate({
-			scrollTop: 0 ,
-		 	}, scroll_top_duration
-		);
-	});
+	    window.requestAnimationFrame(animateScroll);
+	}
 
-});
+	Math.easeInOutQuad = function (t, b, c, d) {
+ 		t /= d/2;
+		if (t < 1) return c/2*t*t + b;
+		t--;
+		return -c/2 * (t*(t-2) - 1) + b;
+	};
+
+	//class manipulations - needed if classList is not supported
+	function hasClass(el, className) {
+	  	if (el.classList) return el.classList.contains(className);
+	  	else return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+	}
+	function addClass(el, className) {
+		var classList = className.split(' ');
+	 	if (el.classList) el.classList.add(classList[0]);
+	 	else if (!hasClass(el, classList[0])) el.className += " " + classList[0];
+	 	if (classList.length > 1) addClass(el, classList.slice(1).join(' '));
+	}
+	function removeClass(el, className) {
+		var classList = className.split(' ');
+	  	if (el.classList) el.classList.remove(classList[0]);	
+	  	else if(hasClass(el, classList[0])) {
+	  		var reg = new RegExp('(\\s|^)' + classList[0] + '(\\s|$)');
+	  		el.className=el.className.replace(reg, ' ');
+	  	}
+	  	if (classList.length > 1) removeClass(el, classList.slice(1).join(' '));
+	}
+})();
